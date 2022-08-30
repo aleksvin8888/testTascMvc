2a149034432e5f2911f0a176e595a2ad57dfc244
Oleksandr Komarnitskyi <aleksvin8888@gmail.com>
2022-08-30 15:40:27 +0300

    add seeders fo user and departments

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Department;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
@@ -22,12 +23,20 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+
+        $departments = Department::select('id')->get();
+
+
         return [
-            'name' => $this->faker->name,
+            'first_name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '123456789', // password
             'remember_token' => Str::random(10),
+            'address' => $this->faker->address,
+            'phone' => $this->faker->phoneNumber,
+            'comment' => $this->faker->text(20),
+            'department_id' => $departments->random()
         ];
     }
 }
--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
 use Illuminate\Database\Seeder;
 
 class DepartmentSeeder extends Seeder
@@ -13,6 +14,61 @@ class DepartmentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $departments = [
+            [
+                'title' => 'CEO'
+            ],
+            [
+                'title' => 'Deputy General Director'
+            ],
+            [
+                'title' => 'Emergency Management Department'
+            ],
+            [
+                'title' => 'Emergency Response Department'
+            ],
+            [
+                'title' => 'Search service'
+            ],
+            [
+                'title' => 'Volunteer development sector'
+            ],
+            [
+                'title' => 'Department of First Aid'
+            ],
+            [
+                'title' => 'Fundraiser'
+            ],
+            [
+                'title' => 'Information and Public Relations Department'
+            ],
+            [
+                'title' => 'Personnel development sector'
+            ],
+            [
+                'title' => 'Legal sector'
+            ],
+            [
+                'title' => 'Department of Cooperation and International Relations'
+            ],
+            [
+                'title' => 'Logistics and Procurement Department'
+            ],
+            [
+                'title' => 'Department of Health and Welfare'
+            ],
+            [
+                'title' => 'Department of psychosocial support'
+            ],
+            [
+                'title' => 'Information center'
+            ],
+        ];
+
+
+
+        foreach($departments as $department) {
+            Department::create($department);
+        }
     }
 }
